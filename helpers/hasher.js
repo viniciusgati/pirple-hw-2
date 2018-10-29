1d0e9fefeb5a5d293f8879a8114ab82f21262b78
vinicius <viniciusgati@gmail.com>
2018-10-29 00:04:03 -0200

    camada de persistencia e helper simples de hashing para nao salvar os arquivos com o id direto no banco

---
+++ helpers/hasher.js
@@ -0,0 +1,18 @@
+/*
+ * Library for hashing some string
+ *
+ */
+
+require 'crypto'
+
+let lib.hash = function(str){
+  if(typeof(str) == 'string' && str.length > 0){
+    var hash = crypto.createHmac('sha256', config.hashingSecret).update(str).digest('hex');
+    return hash;
+  } else {
+    return false;
+  }
+}
+
+// Create a SHA256 hash
+module.exports = lib
