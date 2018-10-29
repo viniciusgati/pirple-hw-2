ce1a91ed5156705a830dfacf47461c925064fb31
vinicius <viniciusgati@gmail.com>
2018-10-29 00:04:35 -0200

    metodo de buscar usuario criado, falta testar pois precisa fazeer o de criar primeiro

--- handlers/users_handlers.js
+++ handlers/users_handlers.js
@@ -1,24 +1,53 @@
+const db = require('../helpers/persistance')
+const hasher = require('../helpers/hasher')
+
 // Define users handlers
 let handlers = {
     users: {}
 }
 
-console.log('loading users handlers')
+console.info('loading users handlers')
 
 //send not found message if user requested anything else
-handlers.users.get = (_, callback) => {
-    callback(404,{
-        'error': 'yes get users man',
-        'error-code': 404,
-    });
-};
-
-handlers.users.post = (_, callback) => {
-    callback(406, {
-        'error': 'yes post users man',
-        'error-code': 406
+handlers.users.get = (payload, callback) => {
+    let hashed_id = hasher.hash(payload.id.toString())
+    db.read(hashed_id, (err, data) => {
+        if(err)
+            callback(404, {
+                'error': 'user not found, maybe create a new one :)',
+                'callback-error': err,
+                'error-code': 404
+            })
+        else
+            callback(200, {
+                'status': 'ok',
+                'user': data,
+                'error-code': null
+            })
     })
 }
 
-module.exports = handlers;
+handlers.users.post = (payload, callback) => {
+    // validates the model
+    var password = data.payload.password.trim()
+    var password = typeof(data.payload.password) == 'string' && data.payload.password.trim().length == 11 ? data.payload.password : false;
 
+    // verify if it exists, if yes message 'please use put'
+
+    // else create the user
+
+    callback(201,{
+        'status': 'created',
+        'error-code': null,
+    })
+}
+
+handlers.users.put = (payload, callback) => {
+
+}
+
+handlers.users.delete = (payload, callback) => {
+
+}
+
+module.exports = handlers;
